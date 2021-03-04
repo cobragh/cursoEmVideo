30434a50835053f0fd0397e00aa778fd08bc93df
cobra <lucas_alvesf09@outlook.com>
2021-03-04 16:44:46 -0300

    Programinha de teste para Git

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,5 @@ public class Main {
         note.ligar();
         note.status();
         note.jogar();
-
     }
 }
--- src/com/company/Notebook.java
+++ src/com/company/Notebook.java
@@ -85,5 +85,6 @@ public class Notebook {
             System.out.println("Pode programar a vontade!");
         }
     }
+
 }
 
